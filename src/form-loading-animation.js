8f3fcdb7dd2b3ea2a7f1eb0af715e8f48714a38b
Pascale Beier <mail@pascalebeier.de>
2017-06-08 11:28:16 +0200

    Fallback if no form avail

--- src/form-loading-animation.js
+++ src/form-loading-animation.js
@@ -1,12 +1,22 @@
 module.exports = () => {
+    const animate = element => {
+        if (element.querySelector('i')) {
+            element.querySelector('i').setAttribute('class', '');
+            element.querySelector('i').classList.add('fa', 'fa-circle-o-notch', 'fa-spin');
+        } else {
+            element.innerHTML = `<i class="fa fa-circle-o-notch fa-spin"></i> ${button.textContent}`;
+        }
+    };
+
     document.querySelectorAll('[data-form-loading-animation]').forEach(button => {
-        button.form.addEventListener('submit', event => {
-            if (button.querySelector('i')) {
-                button.querySelector('i').setAttribute('class', '');
-                button.querySelector('i').classList.add('fa', 'fa-circle-o-notch', 'fa-spin');
-            } else {
-                button.innerHTML = `<i class="fa fa-circle-o-notch fa-spin"></i> ${button.textContent}`;
-            }
-        });
+        if (button.form) {
+            button.form.addEventListener('submit', event => {
+                animate(button);
+            })
+        } else {
+            button.addEventListener('click', event => {
+                animate(button);
+            })
+        }
     });
 };
