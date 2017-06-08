7c7cb26846f8fe18e423784320b76d7b71152523
Pascale Beier <mail@pascalebeier.de>
2017-06-08 11:20:42 +0200

    Switch to data api

--- src/form-loading-animation.js
+++ src/form-loading-animation.js
@@ -1,10 +1,12 @@
-module.exports = button => {
-    button.form.addEventListener('submit', event => {
-        if (button.querySelector('i')) {
-            button.querySelector('i').setAttribute('class', '');
-            button.querySelector('i').classList.add('fa', 'fa-circle-o-notch', 'fa-spin');
-        } else {
-            button.innerHTML = `<i class="fa fa-circle-o-notch fa-spin"></i> ${button.textContent}`;
-        }
+module.exports = () => {
+    document.querySelectorAll('[data-form-loading-animation]').forEach(button => {
+        button.form.addEventListener('submit', event => {
+            if (button.querySelector('i')) {
+                button.querySelector('i').setAttribute('class', '');
+                button.querySelector('i').classList.add('fa', 'fa-circle-o-notch', 'fa-spin');
+            } else {
+                button.innerHTML = `<i class="fa fa-circle-o-notch fa-spin"></i> ${button.textContent}`;
+            }
+        });
     });
 };
